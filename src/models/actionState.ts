453433799a464a9289fc14b7968654773f947932
Rellyson Douglas <rellysondouglas2015@gmail.com>
2022-06-05 16:12:08 -0300

    add actionState model

---
+++ src/models/actionState.ts
@@ -0,0 +1,78 @@
+export type ActionStateType = {
+  title: string,
+  states: string[],
+  stateIndex: number,
+  disabled: boolean,
+}
+
+// export const RobotStateActions = {
+//   ARMS: {
+//     title: "BRAÇOS",
+//     description: "Controle dos movimentos dos braços do robô.",
+//     content: {
+//       LEFT_ELBOW: {
+//         title: "Cotovelo Esquerdo",
+//         states: [
+//           'Em Repouso',
+//           'Levemente contraído',
+//           'Contraído',
+//           'Fortemente contraído',
+//         ],
+//         stateIndex: 0,
+//       },
+//       RIGHT_ELBOW: {
+//         title: "Cotovelo Direito",
+//         states: [
+//           'Em Repouso',
+//           'Levemente contraído',
+//           'Contraído',
+//           'Fortemente contraído',
+//         ],
+//         stateIndex: 0,
+//       },
+//       LEFT_PULSE: {
+//         title: "Pulso Esquerdo",
+//         states: [
+//           'Rotação para -90°',
+//           'Rotação para -45°',
+//           'Em Repouso',
+//           'Rotação para 45°',
+//           'Rotação para 90°',
+//           'Rotação para 135°',
+//           'Rotação para 180°',
+//         ],
+//         stateIndex: 0,
+//       },
+//       RIGHT_PULSE: {
+//         title: "Pulso Direito",
+//         states: [
+//           'Rotação para -90°',
+//           'Rotação para -45°',
+//           'Em Repouso',
+//           'Rotação para 45°',
+//           'Rotação para 90°',
+//           'Rotação para 135°',
+//           'Rotação para 180°',
+//         ],
+//         stateIndex: 0,
+//       },
+//     }
+//   },
+//   HEAD: {
+//     title: "CABEÇA",
+//     description: "Controle dos movimentos da cabeça do robô.",
+//     content: {
+//       HEAD_ROTATION: {
+//         title: "Cabeça",
+//         states: [
+//           'Em Repouso',
+//           'Levemente contraído',
+//           'Contraído',
+//           'Fortemente contraído',
+//         ],
+//         stateIndex: 0,
+//       },
+//     }
+//   }
+
+// }
