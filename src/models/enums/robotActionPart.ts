edb47a0b5865e14c605e9224a1dfeac5e3e7c434
Rellyson Douglas <rellysondouglas2015@gmail.com>
2022-06-05 16:15:09 -0300

    add robot action part enum

---
+++ src/models/enums/robotActionPart.ts
@@ -0,0 +1,8 @@
+export enum ROBOT_ACTION_PART {
+  LEFT_ELBOW = "leftElbow",
+  RIGHT_ELBOW = "rightElbow",
+  LEFT_PULSE = "leftPulse",
+  RIGHT_PULSE = "rightPulse",
+  HEAD_ROTATION = "headRotation",
+  HEAD_INCLINATION = "headInclination",
+}
