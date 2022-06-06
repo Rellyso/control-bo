2970da206fa366dc0a35b3f2310c714ca75dbca6
Rellyson Douglas <rellysondouglas2015@gmail.com>
2022-06-06 13:59:40 -0300

    update action button to add disabled

--- src/components/ControlCard/ActionButtonControlCard.tsx
+++ src/components/ControlCard/ActionButtonControlCard.tsx
@@ -4,12 +4,18 @@ import { ReactNode } from "react"
 type ActionButtonControlCardTypes = {
   icon: ReactNode;
   id?: string;
-  onClick?: React.MouseEventHandler<HTMLButtonElement>
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
+  disabled?: boolean;
 }
 
-export function ActionButtonControlCard({ icon, onClick, id }: ActionButtonControlCardTypes) {
+export function ActionButtonControlCard({ icon, onClick, id, disabled = false }: ActionButtonControlCardTypes) {
   return (
-    <button onClick={onClick} id={id} className="flex w-full items-center justify-center hover:bg-slate-600/25 p-2 transition-colors rounded-lg focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500">
+    <button
+      onClick={onClick}
+      id={id}
+      className="text-slate-50 flex w-full items-center justify-center hover:bg-slate-600/25 p-2 transition-colors rounded-lg focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 disabled:text-zinc-600/25 disabled:hover:bg-slate-700/25 disabled:cursor-not-allowed"
+      disabled={disabled}
+    >
       {icon}
     </button>
   )
