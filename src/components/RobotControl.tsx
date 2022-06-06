9f5e5893c5d081ccfe2be57b3c1aa4269c4ba317
Rellyson Douglas <rellysondouglas2015@gmail.com>
2022-06-06 14:01:36 -0300

    add all cards, informations and styles

--- src/components/RobotControl.tsx
+++ src/components/RobotControl.tsx
@@ -1,33 +1,47 @@
-import { CaretDown, CaretUp } from "phosphor-react";
 import { useRobotActions } from "../hooks/useRobotActions";
 
 import { ControlCard } from "./ControlCard";
-import { ActionButtonControlCard } from "./ControlCard/ActionButtonControlCard";
-import { Header } from "./Header";
 
 
 export function RobotControl() {
 
-  const { headInclination } = useRobotActions()
+  const { leftElbow, rightElbow, leftPulse, rightPulse, headInclination, headRotation } = useRobotActions()
 
   return (
-    <div className="bg-slate-900 min-h-screen">
-      <Header />
+    <div className="container">
 
-      <div className="container mx-4 text-xl font-middle">
-        <h2 className="mt-4 text-slate-50">
-          Controle de ações projeto Robô
-        </h2>
-      </div>
+      <main className="relative overflow-auto p-4">
 
-      <main className="relative overflow-auto m-4">
+        <article className="flex flex-col mt-20">
+          <h2 className="dark:text-white text-3xl font-bold">Braços</h2>
+          <p className="text-slate-400 text-xl font-normal mb-6">Controle dos movimentos dos braços do robô</p>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <ControlCard
+              robotAction={leftElbow}
+            />
+            <ControlCard
+              robotAction={rightElbow}
+            />
+            <ControlCard
+              robotAction={leftPulse}
+            />
+            <ControlCard
+              robotAction={rightPulse}
+            />
+          </div>
+        </article>
 
-        <article className="">
-          <h2>Cabeça</h2>
-          <p>Controle dos movimentos dos braços do robô</p>
-          <ControlCard
-            robotAction={headInclination}
-          />
+        <article className="flex flex-col mt-20">
+          <h2 className="dark:text-white text-3xl font-bold">Cabeça</h2>
+          <p className="text-slate-400 text-xl font-normal mb-6">Controle dos movimentos da cabeça do robô</p>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <ControlCard
+              robotAction={headInclination}
+            />
+            <ControlCard
+              robotAction={headRotation}
+            />
+          </div>
         </article>
 
 
