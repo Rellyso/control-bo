2299c0a4f068ce558b9b4c038daa13dc3c8d2e93
Rellyson Douglas <rellysondouglas2015@gmail.com>
2022-06-06 14:03:48 -0300

    update to set working robot actions context: - using use-selector-context lib - initializing using localstorage - added rules of moviments

--- src/contexts/RobotActionsProvider.tsx
+++ src/contexts/RobotActionsProvider.tsx
@@ -1,12 +1,14 @@
-import { createContext, ReactNode, useState } from "react"
+import { ReactNode, useEffect, useState } from "react"
+import { createContext } from "use-context-selector"
 import { ActionStateType } from "../models/actionState"
 import { ROBOT_ACTION_PART } from "../models/enums/robotActionPart";
-import { getNewLeftElbow } from "../models/leftElbow";
+import { getHeadInclination } from "../models/headInclination";
+import { getHeadRotation } from "../models/headRotation";
+import { getLeftElbow } from "../models/leftElbow";
+import { getLeftPulse } from "../models/leftPulse";
+import { getRightElbow } from "../models/rightElbow";
+import { getRightPulse } from "../models/rightPulse";
 
-type ChangeRobotActionStateProps = {
-  newStateIndex: number,
-  robotActionPart: ROBOT_ACTION_PART,
-}
 
 type RobotActionsProviderData = {
   leftElbow: ActionStateType;
@@ -15,10 +17,9 @@ type RobotActionsProviderData = {
   rightPulse: ActionStateType;
   headRotation: ActionStateType;
   headInclination: ActionStateType;
-  changeRobotActionState: ({
-    newStateIndex,
-    robotActionPart
-  }: ChangeRobotActionStateProps) => void
+  changeRobotActionState: (
+    newRobotActionState: ActionStateType
+  ) => void
 }
 
 
@@ -29,74 +30,122 @@ type RobotActionsProviderProps = {
 }
 export const RobotActionsProvider = ({ children }: RobotActionsProviderProps) => {
 
-  const [leftElbow, setLeftElbow] = useState<ActionStateType>(getNewLeftElbow())
-  const [rightElbow, setRightElbow] = useState<ActionStateType>(getNewLeftElbow())
-  const [leftPulse, setLeftPulse] = useState<ActionStateType>(getNewLeftElbow())
-  const [rightPulse, setRightPulse] = useState<ActionStateType>(getNewLeftElbow())
-  const [headRotation, setHeadRotation] = useState<ActionStateType>(getNewLeftElbow())
-  const [headInclination, setHeadInclination] = useState<ActionStateType>(getNewLeftElbow())
-
-  // criar useEffect para recuperar estados salvos
-
+  const [leftElbow, setLeftElbow] = useState<ActionStateType>(getLeftElbow())
+  const [rightElbow, setRightElbow] = useState<ActionStateType>(getRightElbow())
+  const [leftPulse, setLeftPulse] = useState<ActionStateType>(getLeftPulse())
+  const [rightPulse, setRightPulse] = useState<ActionStateType>(getRightPulse())
+  const [headRotation, setHeadRotation] = useState<ActionStateType>(getHeadRotation())
+  const [headInclination, setHeadInclination] = useState<ActionStateType>(getHeadInclination())
 
   // criar useEffect para adicionar restrições de movimentos
+  useEffect(() => {
+    handleLeftPulse();
+    handleRightPulse();
+    handleHeadRotation();
+  }, [leftElbow, rightElbow, headInclination])
 
-  function changeRobotActionState({ newStateIndex, robotActionPart }: ChangeRobotActionStateProps) {
-    switch (robotActionPart) {
+
+  function changeRobotActionState(newRobotActionState: ActionStateType) {
+
+    // modificar estado
+    switch (newRobotActionState.robotActionPart) {
       case ROBOT_ACTION_PART.LEFT_ELBOW:
-        setLeftElbow({
-          ...leftElbow,
-          stateIndex: newStateIndex
-        })
+        setLeftElbow(() => (newRobotActionState))
         break;
 
       case ROBOT_ACTION_PART.RIGHT_ELBOW:
-        setRightElbow({
-          ...rightElbow,
-          stateIndex: newStateIndex
-        })
+        setRightElbow(() => (newRobotActionState))
         break;
 
       case ROBOT_ACTION_PART.LEFT_PULSE:
-        setLeftPulse({
-          ...leftPulse,
-          stateIndex: newStateIndex
-        })
+        setLeftPulse(() => (newRobotActionState))
         break;
 
       case ROBOT_ACTION_PART.RIGHT_PULSE:
-        setRightPulse({
-          ...rightPulse,
-          stateIndex: newStateIndex
-        })
+        setRightPulse(() => (newRobotActionState))
         break;
 
       case ROBOT_ACTION_PART.HEAD_ROTATION:
-        setHeadRotation({
-          ...headRotation,
-          stateIndex: newStateIndex
-        })
+        setHeadRotation(() => (newRobotActionState))
         break;
 
       case ROBOT_ACTION_PART.HEAD_INCLINATION:
-        setHeadInclination({
-          ...headInclination,
-          stateIndex: newStateIndex
-        })
+        setHeadInclination(() => (newRobotActionState))
+        break;
+
+      default:
         break;
     }
+
+    // adicionar ao armazenamento local
+    localStorage.setItem(newRobotActionState.robotActionPart, JSON.stringify(newRobotActionState))
+  }
+
+  function handleLeftPulse() {
+    let newLeftPulse = leftPulse
+
+    let isStrongContractedLeftElbow =
+      leftElbow.states[leftElbow.stateIndex] === leftElbow.states.find(state => state === "Fortemente contraído")
+
+
+    if (isStrongContractedLeftElbow) {
+      newLeftPulse.disabled = false
+    }
+
+    if (!isStrongContractedLeftElbow) {
+      newLeftPulse.disabled = true
+    }
+
+    changeRobotActionState(newLeftPulse)
+  }
+
+  function handleRightPulse() {
+    let newRightPulse = rightPulse
+
+    let isStrongContractedRightElbow =
+      rightElbow.states[rightElbow.stateIndex] === rightElbow.states.find(state => state === "Fortemente contraído")
+
+
+    if (isStrongContractedRightElbow) {
+      newRightPulse.disabled = false
+    }
+
+    if (!isStrongContractedRightElbow) {
+      newRightPulse.disabled = true
+    }
+
+    changeRobotActionState(newRightPulse)
+  }
+
+  function handleHeadRotation() {
+    let newHeadRotation = headRotation
+
+    let canRotateHead =
+      headInclination.states[headInclination.stateIndex] === headInclination.states.find(state => state === "Para baixo")
+
+    console.log(canRotateHead)
+
+    if (canRotateHead) {
+      newHeadRotation.disabled = true
+    }
+
+    if (!canRotateHead) {
+      newHeadRotation.disabled = false
+    }
+
+    changeRobotActionState(newHeadRotation)
   }
 
   return (
     <RobotActionsContext.Provider
       value={{
-        leftElbow,
-        rightElbow,
-        leftPulse,
-        rightPulse,
-        headRotation,
-        headInclination,
-        changeRobotActionState
+        leftElbow: leftElbow,
+        rightElbow: rightElbow,
+        leftPulse: leftPulse,
+        rightPulse: rightPulse,
+        headRotation: headRotation,
+        headInclination: headInclination,
+        changeRobotActionState: changeRobotActionState
       }}
     >
       {children}
