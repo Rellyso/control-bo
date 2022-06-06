c636858489fe96222b4d6162da8c15f9cf9d8b7e
Rellyson Douglas <rellysondouglas2015@gmail.com>
2022-06-06 14:00:26 -0300

    created Footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,16 @@
+export function Footer() {
+  return (
+    <footer id="footer" className="bg-slate-900 flex items-center justify-center h-20 p-2 border-t border-slate-300/10">
+      <p className="text-slate-300 font-normal text-lg">
+        criado com ❤️ por
+        <a
+          href="https://www.instagram.com/rellysondouglaas/"
+          target="_blank"
+          className="text-slate-50 underline ml-1 bg-zinc-900 p-1"
+        >
+          @rellysondouglaas
+        </a>
+      </p>
+    </footer>
+  )
+}
